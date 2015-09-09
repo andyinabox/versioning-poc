a85f31cebc945c180dde6bdc393bff96e692e60e
Andy Dayton <amdayton@gmail.com>
2015-09-09 14:02:32 -0400

    Updated task aliases

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,12 +24,13 @@ module.exports = function(grunt) {
 
 	grunt.registerTask('build', []);
 	grunt.registerTask('default', ['build']);
-	grunt.registerTask('release-prep', ['checkbranch', 'build']);
+	grunt.registerTask('release-prep', ['checkbranch:master', 'build']);
+
+	// release task aliases
 	grunt.registerTask('release', ['release-prep', 'imprint-release']);
 	grunt.registerTask('release:patch', ['release-prep', 'imprint-release:patch']);
 	grunt.registerTask('release:minor', ['release-prep', 'imprint-release:minor']);
 	grunt.registerTask('release:major', ['release-prep', 'imprint-release:major']);
-
-
+	grunt.registerTask('release:prerelease', ['release-prep', 'imprint-release:prerelease']);
 
 }
